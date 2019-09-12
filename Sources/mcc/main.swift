2ab3353357965acc5e42d83cd06f6b8182409982
Emir Sarı <emirsari@gmail.com>
2019-09-12 13:47:44 +0300

    improved help

--- Sources/mcc/main.swift
+++ Sources/mcc/main.swift
@@ -4,7 +4,6 @@
 
 import Foundation
 
-var registry = CommandRegistry(usage: "\n<subcommand> <option> <\"text in brackets\">\n<subcommand> <option> --help",
-                               overview: "Morse Code Converter")
+var registry = CommandRegistry(usage: "\n<subcommand> <option> <\"text in brackets\">\n<subcommand> <option> --help\nTo convert double quotes, either use curly quotes or type plain double quotes with \\\nTo convert an exclamation mark, add \\ before it.", overview: "Morse Code Converter")
 registry.register(command: ConvertCommand.self)
 registry.run()

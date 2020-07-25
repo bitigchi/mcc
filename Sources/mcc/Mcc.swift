67153e86eae3b240340ba23d28b3d0b422a33e36
Emir Sarı <bitigchi@me.com>
2020-07-25 18:44:13 +0300

    Add boilerplate code for localisations & fix formatting

--- Sources/mcc/Mcc.swift
+++ Sources/mcc/Mcc.swift
@@ -9,29 +9,36 @@ import Foundation
 
 struct Mcc: ParsableCommand {
     static let configuration: CommandConfiguration =
-        .init(abstract: NSLocalizedString(
-            "Simple text-to-morse converter", comment: "Program description"),
-              version: "morse code converter, " + NSLocalizedString(
-                    "version ", comment: "Version info") + "0.1.7")
+        .init(abstract: NSLocalizedString("Simple text-to-morse converter",
+                                          bundle: program.bundle ?? .module,
+                                          comment: "Program description"),
+              version: "morse code converter, " +
+                NSLocalizedString("version ",
+                                  bundle: program.bundle ?? .module,
+                                  comment: "Version info") + "0.1.8")
     
     @Argument(help: ArgumentHelp(NSLocalizedString(
-        "The text to convert",
-        comment: "Argument help")))
+                                    "The text to convert.",
+                                    bundle: program.bundle ?? .module,
+                                    comment: "Argument help")))
     var text: String
     
-    @Flag(name: .shortAndLong, help: ArgumentHelp(NSLocalizedString(
-        "Copy output to clipboard (macOS only)",
-        comment: "Argument help")))
+    @Flag(name: .shortAndLong, help: ArgumentHelp(
+            NSLocalizedString("Copy output to clipboard (macOS only).",
+                              bundle: program.bundle ?? .module,
+                              comment: "Argument help")))
     var copy = false
     
-    @Flag(name: .shortAndLong, help: ArgumentHelp(NSLocalizedString(
-        "Only display morse output",
-        comment: "Argument help")))
+    @Flag(name: .shortAndLong, help: ArgumentHelp(
+            NSLocalizedString("Only display morse output.",
+                              bundle: program.bundle ?? .module,
+                              comment: "Argument help")))
     var morseOnly = false
     
-    @Flag(name: .shortAndLong, help: ArgumentHelp(NSLocalizedString(
-        "Strip whitespace from output",
-        comment: "Argument help")))
+    @Flag(name: .shortAndLong, help: ArgumentHelp(
+            NSLocalizedString("Strip whitespace from output.",
+                              bundle: program.bundle ?? .module,
+                              comment: "Argument help")))
     var whitespace = false
     
     func run() throws {
@@ -44,4 +51,18 @@ struct Mcc: ParsableCommand {
                                        morseOnly: morseOnly)
         consoleIO.writeMessage(result)
     }
+}
+
+final class Program {
+    let bundle = program.localizationBundle(
+        forLanguage: Locale.current.languageCode ?? "en")
+
+    func localizationBundle(forLanguage language: String) -> Bundle? {
+         if let path = Bundle.module.path(forResource: language,
+                                          ofType: "lproj") {
+             return Bundle(path: path)
+         } else {
+             return nil
+         }
+     }
 }
--- Sources/mcc/main.swift
+++ Sources/mcc/main.swift
@@ -5,4 +5,9 @@
 
 import Foundation
 
+let program = Program()
 Mcc.main()
+
+
+
+
